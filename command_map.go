89a2741ac1d27647ab319dfa1f2bcbb489ee7175
furkansoyturk <furkansoyturk@icloud.com>
2024-05-01 23:13:18 +0300

    OKKKKKK

--- command_map.go
+++ command_map.go
@@ -32,13 +32,3 @@ func commandMapB(cfg *config, args ...string) error {
 	fmt.Println("")
 	return nil
 }
-func commandExplore(cfg *config, args ...string) error {
-	name := args[0]
-	response := cfg.pokeClient.ExplorePokemons(name)
-	fmt.Println("Following pokemons found:")
-	for _, pokemons := range response.PokemonEncounters {
-		fmt.Printf("%s\n", pokemons.Pokemon.Name)
-	}
-	fmt.Println("")
-	return nil
-}
